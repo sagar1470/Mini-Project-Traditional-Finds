532ccf4a9c35bf845244373a81436695da9e8e37
Sagar <sagar.dev1414@gmail.com>
2025-05-06 17:50:00 +0545

    done

--- backend/config/stripe.js
+++ backend/config/stripe.js
@@ -1,6 +1,6 @@
 
 import Stripe from 'stripe';
 
-const stripe = new Stripe('sk_test_51RKcEIE0LaK0t270mlrS7Ei8jKtBI0613hpZFPGoAdzgQ5gPbzG9Hz8qEyt6jQ2tBpxipp73qDYX3eZciPYxjCx800ynhlGM0C')
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 export default stripe;
